6b3c67a663c7fd0d34c337a438af2c3fad4c34fa
mosabkh12 <159187725+mosabkh12@users.noreply.github.com>
2025-01-18 00:20:34 +0200

    ojnlkm,.l,km

--- Library/Models/BooksAndPaymentsViewModel.cs
+++ Library/Models/BooksAndPaymentsViewModel.cs
@@ -5,7 +5,7 @@ namespace Library.ViewModels
 {
     public class BooksAndPaymentsViewModel
             public IEnumerable<Book> Books { get; set; }
-        public IEnumerable<payment2> Payments { get; set; }
+        public IEnumerable<payment2> Paymentjs { get; set; }
 
         public Book purchasedProduct { get; set; }
         public List<Book> purchased { get; set; }
